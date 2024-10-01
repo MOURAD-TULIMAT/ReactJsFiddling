cc8f60357f988efd0abcc38a5fcf759b008dcf08
mourad tulimat <mourad.tulimat@wizme.com>
2024-10-01 14:43:55 +0300

    fix routing for gh pages try 2

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function AppContent() {
         style={{ width: "215px" }}
       >
         <h4 className="mb-4">Fiddling Items</h4>
-        <Link to="FiddlingGroup/github-explorer">
+        <Link to="githubexplorer">
           <button
             className={`btn ${
               theme === "dark" ? "btn-outline-dark" : "btn-outline-light"
@@ -53,10 +53,7 @@ function AppContent() {
 
         {/* Routes */}
         <Routes>
-          <Route
-            path="FiddlingGroup/github-explorer"
-            element={<GitHubProfileExplorer />}
-          />
+          <Route path="githubexplorer" element={<GitHubProfileExplorer />} />
         </Routes>
       </div>
     </div>
